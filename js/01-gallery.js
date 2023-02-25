cc321fd10eb132ad07e5ce6c9691506312b69133
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-02-25 22:52:12 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+        `;
+    })
+    .join('');
+}
+
 console.log(galleryItems);
