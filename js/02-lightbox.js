bf72af5fb8a89289b03e740079f781f313f7397d
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-02-27 23:29:42 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainerEl = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainerEl.insertAdjacentHTML('beforeend', itemsMarkup);
+
+galleryContainerEl.addEventListener('click', handleGalleryContainerElClick);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+          <a class="gallery__item" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>
+        `;
+    })
+    .join('');
+}
+
+function handleGalleryContainerElClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  new SimpleLightbox('.gallery a', {
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    scrollZoom: false,
+  });
+}
+
 console.log(galleryItems);
